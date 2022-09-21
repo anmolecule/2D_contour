d72fd6bb6eb4bac02147b6c1c0bdd9b97e7fee43
Anmol Kumar <anmolkumarsrivastava@gmail.com>
2022-09-21 13:15:52 -0400

    Update plotcontour_A.py

--- plotcontour_A.py
+++ plotcontour_A.py
@@ -1,5 +1,6 @@
-# Author: Anmol Kumar
-# Date: 02/09/2021
+__author__ = "Anmol Kumar"
+__date__   = "02/09/2021"
+__modified__ = "21/09/2022"
 
 # Usage: python plotcontour_A.py csvfile HeaderContainingEnergy
 
@@ -12,32 +13,42 @@ import matplotlib.tri as tri
 from scipy.interpolate import griddata
 plt.style.use('seaborn-white')
 
-filename=sys.argv[1]
-df=pd.read_csv(filename) 
-df_sorted=df.sort_values(by=['Phi','Psi'])
-df_sorted=df_sorted.reset_index()
-df_sorted=df_sorted.drop(columns=['index'])
-x=df_sorted['Phi'].to_numpy() 
-y=df_sorted['Psi'].to_numpy() 
-z=df_sorted[sys.argv[2]].to_numpy()
-xi = np.linspace(-180, 180, 25)
-yi = np.linspace(-180, 180, 25)
-XI,YI=np.meshgrid(xi,yi)
+def getxyzcontour(df,xch='Phi',ych='Psi',zch=None):
+    df_sorted=df.sort_values(by=['Phi','Psi'])
+    df_sorted=df_sorted.reset_index()
+    df_sorted=df_sorted.drop(columns=['index'])
+    x=df_sorted['Phi'].to_numpy() 
+    y=df_sorted['Psi'].to_numpy() 
+    z=df_sorted[sys.argv[2]].to_numpy()
+    xi = np.linspace(-180, 180, 30)
+    yi = np.linspace(-180, 180, 30)
+    XI,YI=np.meshgrid(xi,yi)
+    zi = griddata((x, y), z, (XI,YI), method='cubic')
+    return (xi,yi,zi)
+ 
+def plotxyzcontour(xi,yi,zi,zch=None):
+    fig, axs = plt.subplots(figsize=(8,8),dpi=600)
+    cp = axs.contour(xi, yi, zi, levels=15, linewidths=0.5, colors='k')
+    cntur = axs.contourf(xi, yi, zi, levels=15, cmap="RdBu")
+    fig.colorbar(cntur, ax=axs)
+    #axs.clabel(cp, inline=True, fontsize=10)
+    axs.set(xlim=(-180, 180), ylim=(-180, 180))
+    axs.set_xticks(np.arange(-180,181,30))
+    axs.set_yticks(np.arange(-180,181,30))
+    axs.set_aspect('equal', adjustable='box')
+    plt.title('Contour Plot for '+zch) #(%d points, %d grid points)' %
+    plt.tight_layout()
+    mzch=zch.replace('/', '')
+    plt.savefig("contour_"+mzch+".png",dpi=600)
+    #plt.show()
+    print ("Successfully saved contour in contour_%s.png(mzch)")
 
-fig, axs = plt.subplots(figsize=(8,8),dpi=300)
-zi = griddata((x, y), z, (XI,YI), method='cubic')
-axs.contour(xi, yi, zi, levels=13, linewidths=0.5, colors='k')
-cntur = axs.contourf(xi, yi, zi, levels=13, cmap="RdBu_r")
-fig.colorbar(cntur, ax=axs)
-axs.set(xlim=(-180, 180), ylim=(-180, 120))
-axs.set_xticks(np.arange(-180,181,30))
-axs.set_yticks(np.arange(-180,121,30))
-axs.set_aspect('equal', adjustable='box')
-plt.title('Contour Plot for '+sys.argv[2]) #(%d points, %d grid points)' %
-plt.tight_layout()
-nk=sys.argv[2].replace('/', '')
-print (nk)
-plt.savefig("testcontour_"+nk+".png",dpi=600)
-#plt.show()
 
-sys.exit()
+if __name__ == "__main__":
+    filename=sys.argv[1]
+    df=pd.read_csv(filename)
+    zch=sys.argv[2]
+    print ("Assuming your csv file column headers contain Phi and Psi")
+    xi,yi,zi=getxyzcontour(df,xch='Phi',ych='Psi',zch=zch)
+    plotxyzcontour(xi,yi,zi,zch=zch)
+    sys.exit()
